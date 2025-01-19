3c3499dbbe986bc13bf852d1f8f3a90b20b0f346
Shamima Ali <shamimaali@shamimas-air.home>
2025-01-18 23:41:55 -0500

    server can respond to web browser

--- server.cpp
+++ server.cpp
@@ -5,8 +5,11 @@
 #include <netdb.h>
 #include <stdint.h>
 #include <arpa/inet.h>
+#include <signal.h>
 
 using namespace std;
+
+
 
 int main() {
     int serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
@@ -15,19 +18,44 @@ int main() {
         return 1;
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr.s_addr);
-    memset(serverAddress.sin_zero, '0', 8); // can igore this line if ye want
+    int sockfd;  
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
 
-    
-    if (bind(serverSocketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        cout << "Error binding to socket. " << strerror(errno) << endl;
-        close(serverSocketFd);
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // use AF_INET6 to force IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP address
+
+    if ((rv = getaddrinfo(NULL, "8080", &hints, &servinfo)) != 0) {
+        cout << stderr << " getaddrinfo: " << gai_strerror(rv) << endl;
         return 1;
     }
 
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((serverSocketFd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("socket");
+            continue;
+        }
+
+        if (bind(serverSocketFd, p->ai_addr, p->ai_addrlen) == -1) {
+            cout << "Error binding to socket. " << strerror(errno) << endl;
+            close(serverSocketFd);
+            continue;;
+        }
+
+        break; // if we get here, we must have connected successfully
+    }
+
+    if (p == NULL) {
+    // looped off the end of the list with no successful bind
+        fprintf(stderr, "failed to bind socket\n");
+        return 1;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
     
     if (listen(serverSocketFd, 5) == -1) {
         cout << "Error listening on socket " << strerror(errno) << endl;
@@ -50,6 +78,9 @@ int main() {
             return 1;
         }
         cout << "Message from client: " << clientBuffer << endl;
+
+        char ackBuff[1024] = "HTTP/1.1 200 OK\r\n\r\nMessage received";
+        send(clientSocket, &ackBuff, strlen(ackBuff), 0);
         close(clientSocket);
 
     }
